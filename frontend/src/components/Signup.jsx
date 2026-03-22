b9bfd2832e5ba2b1ed358dc3b290c0439e1c1fbc
Vinita Parmar <vinitaparamar@gmail.com>
2026-03-22 09:55:52 +0530

    Add files via upload

---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { signupStyles } from "../assets/dummystyles";
+
+const Signup = () => {
+    return (
+        <div>
+          Signup
+        </div>
+    )
+}
+
+export default Signup;
