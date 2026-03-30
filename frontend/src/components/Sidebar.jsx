b2863526063214071c9f187cc1a053223db2e5a7
Vinita Parmar <vinitaparamar@gmail.com>
2026-03-30 20:56:37 +0530

    Add files via upload

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { sidebarStyles } from "../assets/dummystyles";
+import questionsData from '../assets/dummydata';
+
+const Sidebar = () => {
+  return (
+    <div className={sidebarStyles.pageContainer}>
+      
+    </div>
+  );
+};
+
+export default Sidebar;
