d48ca2c243421140ba42cef42e57b830b1eeca7a
Vinita Parmar <vinitaparamar@gmail.com>
2026-03-31 19:48:42 +0530

    Refactor Sidebar component and add technologies

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -46,15 +46,108 @@ const Sidebar = () => {
        id: "css",
       name: "CSS",
       icon: <Layout size={20} />,
-      color: "bg-blue-50 text-orange-600 border-orange-200",
+      color: "bg-blue-50 text-blue-600 border-blue-200",
     },
     {
        id: "js",
       name: "JavaScript",
       icon: <Code size={20} />,
-      color: "bg-yellow-50 text-orange-600 border-orange-200",
+      color: "bg-yellow-50 text-yellow-600 border-yellow-200",
     },
-    {
+     {
+       id: "react",
+      name: "React",
+      icon: <Cpu size={20} />,
+      color: "bg-cyan-50 text-cyan-600 border-cyan-200",
+    },
+     {
+       id: "node",
+      name: "Node.js",
+      icon: <Code size={20} />,
+      color: "bg-green-50 text-green-600 border-green-200",
+    },
+     {
+       id: "mongodb",
+      name: "MongoDB",
+      icon: <Database size={20} />,
+      color: "bg-emerald-50 text-orange-600 border-emerald-200",
+    },
+     {
+       id: "java",
+      name: "Java",
+      icon: <Coffee size={20} />,
+      color: "bg-red-50 text-red-600 border-red-200",
+    },
+     {
+       id: "python",
+      name: "Python",
+      icon: <Terminal size={20} />,
+      color: "bg-indigo-50 text-indigo-600 border-indigo-200",
+    },
+     {
+       id: "cpp",
+      name: "C++",
+      icon: <Code size={20} />,
+      color: "bg-purple-50 text-purple-600 border-purple-200",
+    },
+     {
+       id: "bootstrap",
+      name: "Bootstrap",
+      icon: <Layout size={20} />,
+      color: "bg-pink-50 text-pink-600 border-pink-200",
+    },
+  ];
+
+  const levels = [
+  {
+    id: "basic",
+    name: "Basic",
+    questions: 20,
+    icon: <Star size={16} />,
+    color: "bg-green-50 text-green-600"
+  },
+  {
+    id: "intermediate",
+    name: "Intermediate",
+    questions: 40,
+    icon: <Zap size={16} />,
+    color: "bg-blue-50 text-blue-600",
+  },
+  {
+    id: "advanced",
+    name: "Advanced",
+    questions: 60,
+    icon: <Target size={16} />,
+    color: "bg-purple-50 text-purple-600",
+  },
+];
+
+const handleTechSelect = (techId) => {
+  if (selectedTech === techId) {
+    setSelectedTech(null);
+    setSelectedLevel(null);
+  } else {
+    setSelectedTech(techId);
+    setSelectedLevel(null);
+  }
+
+  setCurrentQuestion(0);
+  setUserAnswers({});
+  setShowResults(false);
+  submittedRef.current = false;
+
+  if (window.innerWidth < 768) setIsSidebarOpen(true);
+
+  setTimeout(() => {
+    const el = asideRef.current?.querySelector(
+      `[data-tech="${techId}"]`
+    );
+    if (el) el.scrollIntoView({ behavior: "smooth", block: "center" });
+  }, 120);
+};
+      
+    
+    
       
       
   
