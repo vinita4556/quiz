f4530ce021eefd34da80a31a757f602c88d63585
Vinita Parmar <vinitaparamar@gmail.com>
2026-03-31 19:32:36 +0530

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -22,6 +22,42 @@ const Sidebar = () => {
    window.addEventListener("resize", handleResize);
    return () => window.removeEventListener("resize", handleResize);
  }, []);
+
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      if (isSidebarOpen) document.body.style.overflow = "hidden";
+      else document.body.style.overflow = "";
+    } else {
+      document.body.style.overflow = "";
+    }
+    return() => {
+      document.body.style.overflow = "";
+    };
+  }, [isSidebarOpen]);
+
+  const technologies = [
+    {
+      id: "html",
+      name: "HTML",
+      icon: <Globe size={20} />,
+      color: "bg-orange-50 text-orange-600 border-orange-200",
+    },
+    {
+       id: "css",
+      name: "CSS",
+      icon: <Layout size={20} />,
+      color: "bg-blue-50 text-orange-600 border-orange-200",
+    },
+    {
+       id: "js",
+      name: "JavaScript",
+      icon: <Code size={20} />,
+      color: "bg-yellow-50 text-orange-600 border-orange-200",
+    },
+    {
+      
+      
+  
      
     
      
